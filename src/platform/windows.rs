5668ab05ecd920602b3d62d44dd737dc489c926e
Ryo Yamashita <qryxip@gmail.com>
2019-04-27 18:50:24 +0900

    WIP: Implement `dimensions_std[in|err]

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -1,5 +1,6 @@
+use winapi::shared::minwindef::DWORD;
 use winapi::um::processenv::GetStdHandle;
-use winapi::um::winbase::STD_OUTPUT_HANDLE;
+use winapi::um::winbase::{STD_ERROR_HANDLE, STD_INPUT_HANDLE, STD_OUTPUT_HANDLE};
 use winapi::um::wincon::GetConsoleScreenBufferInfo;
 use winapi::um::wincon::{CONSOLE_SCREEN_BUFFER_INFO, COORD, SMALL_RECT};
 
@@ -23,29 +24,7 @@ use winapi::um::wincon::{CONSOLE_SCREEN_BUFFER_INFO, COORD, SMALL_RECT};
 /// }
 /// ```
 pub fn dimensions() -> Option<(usize, usize)> {
-    let null_coord = COORD { X: 0, Y: 0 };
-    let null_smallrect = SMALL_RECT {
-        Left: 0,
-        Top: 0,
-        Right: 0,
-        Bottom: 0,
-    };
-
-    let stdout_h = unsafe { GetStdHandle(STD_OUTPUT_HANDLE) };
-    let mut console_data = CONSOLE_SCREEN_BUFFER_INFO {
-        dwSize: null_coord,
-        dwCursorPosition: null_coord,
-        wAttributes: 0,
-        srWindow: null_smallrect,
-        dwMaximumWindowSize: null_coord,
-    };
-
-    if unsafe { GetConsoleScreenBufferInfo(stdout_h, &mut console_data) } != 0 {
-        Some(((console_data.srWindow.Right - console_data.srWindow.Left) as usize,
-                (console_data.srWindow.Bottom - console_data.srWindow.Top) as usize))
-    } else {
-        None
-    }
+    query(STD_INPUT_HANDLE)
 }
 
 /// Query the current processes's output, returning its width and height as a
@@ -67,7 +46,9 @@ pub fn dimensions() -> Option<(usize, usize)> {
 ///     println!("Unable to get term size :(")
 /// }
 /// ```
-pub fn dimensions_stdout() -> Option<(usize, usize)> { dimensions() }
+pub fn dimensions_stdout() -> Option<(usize, usize)> {
+    query(STD_OUTPUT_HANDLE)
+}
 
 /// This isn't implemented for Windows
 /// 
@@ -75,7 +56,9 @@ pub fn dimensions_stdout() -> Option<(usize, usize)> { dimensions() }
 /// 
 /// This function `panic!`s unconditionally with the `unimplemented!`
 /// macro
-pub fn dimensions_stdin() -> Option<(usize, usize)> { unimplemented!() }
+pub fn dimensions_stdin() -> Option<(usize, usize)> {
+    query(STD_INPUT_HANDLE)
+}
 
 /// This isn't implemented for Windows
 /// 
@@ -83,4 +66,32 @@ pub fn dimensions_stdin() -> Option<(usize, usize)> { unimplemented!() }
 /// 
 /// This function `panic!`s unconditionally with the `unimplemented!`
 /// macro
-pub fn dimensions_stderr() -> Option<(usize, usize)> { unimplemented!() }
+pub fn dimensions_stderr() -> Option<(usize, usize)> {
+    query(STD_OUTPUT_HANDLE)
+}
+
+fn query(hdl: DWORD) -> Option<(usize, usize)> {
+    let null_coord = COORD { X: 0, Y: 0 };
+    let null_smallrect = SMALL_RECT {
+        Left: 0,
+        Top: 0,
+        Right: 0,
+        Bottom: 0,
+    };
+
+    let hdl = unsafe { GetStdHandle(hdl) };
+    let mut console_data = CONSOLE_SCREEN_BUFFER_INFO {
+        dwSize: null_coord,
+        dwCursorPosition: null_coord,
+        wAttributes: 0,
+        srWindow: null_smallrect,
+        dwMaximumWindowSize: null_coord,
+    };
+
+    if unsafe { GetConsoleScreenBufferInfo(hdl, &mut console_data) } != 0 {
+        Some(((console_data.srWindow.Right - console_data.srWindow.Left) as usize,
+                (console_data.srWindow.Bottom - console_data.srWindow.Top) as usize))
+    } else {
+        None
+    }
+}
